24f385b59ab930d9275ce580dabe4fc77c6d8cd1
Francis <francisgbohunmi@gmail.com>
2024-04-02 19:14:02 +0100

    first commit

---
+++ BaseLibrary/Entities/Inmate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLibrary.Entities
+{
+    public class Inmate : BaseEntity
+    {
+        public string? CivilId { get; set; }
+        public string? FileNumber { get; set; }
+        public string? FullName { get; set; }
+        public string? Offence { get; set; }
+        public string? Address { get; set; }
+        public string? YearsToSpend { get; set; }
+        public string? TelephoneNumber { get; set; }
+        public string? Mugshot { get; set; }    
+        public string? Other { get; set; }
+
+
+
+
+        //Relationship many to one
+
+        public GeneralDepartment? GeneralDepartment { get; set; }
+        public int GeneralDepartmentId { get; set; }
+
+        public Department? Department { get; set; }
+        public int DepartmentId { get; set; }
+
+
+    }
+}
---
+++ ServerLibrary/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibrary.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): DbContext(options)
+        {
+            public DbSet<Inmate> Inmates { get; set; }
+            public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+        }
+    }
+}
